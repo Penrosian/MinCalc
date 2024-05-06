e2e3fac328114a885b9388a46377190cfccea296
Penrosian <106357770+Penrosian@users.noreply.github.com>
2024-05-06 17:52:12 -0400

    Condensed code where possible and split calculator into vanilla and Exogenesis calculators.

--- MinCalc/MinCalc.py
+++ MinCalc/MinCalc.py
@@ -1,17 +1,13 @@
 from math import *
 from sys import *
 
-def cryoMixerToImpact(mixers):
-    return((mixers*12)//15)
+def cryoMixerToImpact(mixers): return((mixers*12)//15)
 
-def blastMixerToImpact(blastMixers):
-    return(int((blastMixers*0.75)//(1/2.33)))
+def blastMixerToImpact(blastMixers): return(int((blastMixers*0.75)//(1/2.33)))
 
-def pyraFacToBlastFac(pyraFacs):
-    return(int((pyraFacs*2.25)//3))
+def pyraFacToBlastFac(pyraFacs): return(int((pyraFacs*2.25)//3))
 
-def blastFacToImpact(blastFacs):
-    return(int((blastFacs*2.25)//(1/2.33)))
+def blastFacToImpact(blastFacs): return(int((blastFacs*2.25)//(1/2.33)))
 
 def surgeCalc(silicon,copper,lead,titanium):
     global siliconRun
@@ -32,26 +28,32 @@ def surgeCalc(silicon,copper,lead,titanium):
     titaniumExcess = round(titanium%1.6, 1)
     if (titaniumRun == leadRun) & (titaniumRun == copperRun) & (titaniumRun == siliconRun): return(1)
     else: return(0)
-
+    
 def ask():
-    asked = int(input("What would you like to calculate?\n(1) Cryofluid mixers to impact reactors\n(2) Blast mixers to impact reactors\n(3) Surge production\n(4) Pyratite factories(Exogenesis) to blast factories(Exogenesis)\n(5) Blast factories(Exogenesis) to impact reactors\n(6) Pyratite factories(Exogenesis) to impact reactors\n"))
+    asked = int(input("What calculator would you like to launch?\n(1) Vanilla\n(2) Exogenesis\n"))
     if asked == 1:
-        print("You can run", str(cryoMixerToImpact(int(input("How many cryofluid mixers? ")))), "reactors.")
+        askVanilla()
     elif asked == 2:
-        print("You can run", str(blastMixerToImpact(int(input("How many blast mixers? ")))), "reactors.")
+        askExogenesis()
+
+def askVanilla():
+    asked = int(input("What would you like to calculate?\n(0) Back\n(1) Cryofluid mixers to impact reactors\n(2) Blast mixers to impact reactors\n(3) Surge production\n"))
+    if asked == 0: ask()
+    elif asked == 1: print("You can run", str(cryoMixerToImpact(int(input("How many cryofluid mixers? ")))), "reactors.")    
+    elif asked == 2: print("You can run", str(blastMixerToImpact(int(input("How many blast mixers? ")))), "reactors.")
     elif asked == 3:
-        if surgeCalc(float(input("How much silicon production do you have? ")),float(input("How much copper production do you have? ")),float(input("How much lead production do you have? ")),float(input("How much titanium production do you have? "))) == 1:
-            print("All resources are able to run the same number of surge smelters. Good job!\nResource excess:",titaniumExcess,"titanium,",leadExcess,"lead,",copperExcess,"copper, and",siliconExcess,"silicon.")
-        else:
-            print("Your resources are not in equilibrium. Your resources can run... Titanium:",int(titaniumRun),"Copper:",int(copperRun),"Lead:",int(leadRun),"Silicon:",int(siliconRun),"surge smelters.\nResource excess:",titaniumExcess,"titanium,",leadExcess,"lead,",copperExcess,"copper, and",siliconExcess,"silicon.")
-    elif asked == 4:
-        print("You can run", str(pyraFacToBlastFac(int(input("How many pyratite factories? ")))), "blast factories.")
-    elif asked == 5:
-        print("You can run", str(blastFacToImpact(int(input("How many blast factories? ")))), "reactors.")
-    elif asked == 6:
-        print("You can run", str(blastFacToImpact(pyraFacToBlastFac(int(input("How many pyratite factories? "))))), "reactors.")
-    for x in range(3):
-        print("")
-    ask()
+        if surgeCalc(float(input("How much silicon production do you have? ")),float(input("How much copper production do you have? ")),float(input("How much lead production do you have? ")),float(input("How much titanium production do you have? "))) == 1: print("All resources are able to run the same number of surge smelters. Good job!\nResource excess:",titaniumExcess,"titanium,",leadExcess,"lead,",copperExcess,"copper, and",siliconExcess,"silicon.")       
+        else: print("Your resources are not in equilibrium. Your resources can run... Titanium:",int(titaniumRun),"Copper:",int(copperRun),"Lead:",int(leadRun),"Silicon:",int(siliconRun),"surge smelters.\nResource excess:",titaniumExcess,"titanium,",leadExcess,"lead,",copperExcess,"copper, and",siliconExcess,"silicon.")   
+    for x in range(3): print("")
+    askVanilla()
+    
+def askExogenesis():
+    asked = int(input("What would you like to calculate?\n(0) Back\n(1) Pyratite factories to blast factories\n(2) Blast factories to impact reactors\n(3) Pyratite factories to impact reactors\n"))
+    if asked == 0: ask()
+    elif asked == 1: print("You can run", str(pyraFacToBlastFac(int(input("How many pyratite factories? ")))), "blast factories.")
+    elif asked == 2: print("You can run", str(blastFacToImpact(int(input("How many blast factories? ")))), "reactors.")
+    elif asked == 3: print("You can run", str(blastFacToImpact(pyraFacToBlastFac(int(input("How many pyratite factories? "))))), "reactors.")   
+    for x in range(3): print("")
+    askExogenesis()
         
 ask()
