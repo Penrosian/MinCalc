44740c6a75f251b169d8ef26e68d5695e8565ca5
Penrosian <106357770+Penrosian@users.noreply.github.com>
2024-05-10 19:43:49 -0400

    Added thorium to RTG. Fixed result becoming a tuple instead of a string.

--- MinCalc/MinCalc.py
+++ MinCalc/MinCalc.py
@@ -38,6 +38,8 @@ def airblastBoost(production): return(round((production*3.24*1.5), 2))
 def goliathBoost(production): return(round((production*9*1.5), 2))
 
 def MPDBoost(production): return(round((production*2.25*1.5), 2))
+
+def thoriumToRTG(thorium): return((thorium*14)//1)
     
 def ask():
     try: asked = int(input("What calculator would you like to launch?\n(1) Vanilla\n(2) Exogenesis\n"))
@@ -53,7 +55,7 @@ def ask():
 
 def askVanilla():
     global saved
-    try: asked = int(input("What would you like to calculate?\n(0) Back\n(1) Cryofluid mixers to impact reactors\n(2) Blast mixers to impact reactors\n(3) Surge production\n(4) Cryofluid mixers to thorium reactors\n(5) Thorium to thorium reactors\n(6) Airblast drill boost\n"))
+    try: asked = int(input("What would you like to calculate?\n(0) Back\n(1) Cryofluid mixers to impact reactors\n(2) Blast mixers to impact reactors\n(3) Surge production\n(4) Cryofluid mixers to thorium reactors\n(5) Thorium to thorium reactors\n(6) Airblast drill boost\n(7) Thorium to RTG\n"))
     except: 
         print("Please input a number.")
         askVanilla()
@@ -63,14 +65,15 @@ def askVanilla():
             saved = result
             askVanilla()
         elif asked == -2: result = saved
-        elif asked == 1: result ="You can run", str(cryoMixerToImpact(int(input("How many cryofluid mixers? ")))), "reactors."    
-        elif asked == 2: result = "You can run", str(blastMixerToImpact(int(input("How many blast mixers? ")))), "reactors."
+        elif asked == 1: result ="You can run" + str(cryoMixerToImpact(int(input("How many cryofluid mixers? ")))) + "reactors."    
+        elif asked == 2: result = "You can run" + str(blastMixerToImpact(int(input("How many blast mixers? ")))) + "reactors."
         elif asked == 3:
-            if surgeCalc(float(input("How much silicon production do you have? ")),float(input("How much copper production do you have? ")),float(input("How much lead production do you have? ")),float(input("How much titanium production do you have? "))) == 1: result = "All resources are able to run the same number of surge smelters. Good job!\nResource excess:",titaniumExcess,"titanium,",leadExcess,"lead,",copperExcess,"copper, and",siliconExcess,"silicon."       
-            else: result = "Your resources are not in equilibrium. Your resources can run... Titanium:",int(titaniumRun),"Copper:",int(copperRun),"Lead:",int(leadRun),"Silicon:",int(siliconRun),"surge smelters.\nResource excess:",titaniumExcess,"titanium,",leadExcess,"lead,",copperExcess,"copper, and",siliconExcess,"silicon."
-        elif asked == 4: result = "You can run", str(int(cryoMixerToThorReact(int(input("How many cryofluid mixers? "))))), "reactors."
-        elif asked == 5: result = "You can run", str(int(thoriumToThorReact(float(input("How much thorium? "))))), "reactors."
-        elif asked == 6: result = "You will produce", str(airblastBoost(float(input("How much does the drill produce? ")))), "items p/sec."
+            if surgeCalc(float(input("How much silicon production do you have? ")),float(input("How much copper production do you have? ")),float(input("How much lead production do you have? ")),float(input("How much titanium production do you have? "))) == 1: result = "All resources are able to run the same number of surge smelters. Good job!\nResource excess: " + str(titaniumExcess) + " titanium, " + str(leadExcess) + " lead, " + str(copperExcess) + " copper, and " + siliconExcess + " silicon."       
+            else: result = "Your resources are not in equilibrium. Your resources can run... Titanium: " + str(int(titaniumRun)) + " Copper: " + str(int(copperRun)) + " Lead: " + str(int(leadRun)) + " Silicon: " + str(int(siliconRun)) + " surge smelters.\nResource excess: " + str(titaniumExcess) + " titanium, " + str(leadExcess) + " lead, " + str(copperExcess) + " copper, and " + str(siliconExcess) + " silicon."
+        elif asked == 4: result = "You can run " + str(int(cryoMixerToThorReact(int(input("How many cryofluid mixers? "))))) + " reactors."
+        elif asked == 5: result = "You can run " + str(int(thoriumToThorReact(float(input("How much thorium? "))))) + " reactors."
+        elif asked == 6: result = "You will produce " + str(airblastBoost(float(input("How much does the drill produce? ")))) + " items p/sec."
+        elif asked == 7: result = "You can run " + str(int(thoriumToRTG(float(input("How much thorium? "))))) + " generators."
         else: 
             print("Please select an option. Input 0 to go back.")
             askVanilla()
@@ -90,11 +93,11 @@ def askExogenesis():
             saved = result
             askExogenesis()
         elif asked == -2: result = saved
-        elif asked == 1: result = "You can run", str(pyraFacToBlastFac(int(input("How many pyratite factories? ")))), "blast factories."
-        elif asked == 2: result = "You can run", str(blastFacToImpact(int(input("How many blast factories? ")))), "reactors."
-        elif asked == 3: result = "You can run", str(blastFacToImpact(pyraFacToBlastFac(int(input("How many pyratite factories? "))))), "reactors."   
-        elif asked == 4: result = "The drill will produce", str(goliathBoost(float(input("How much does the drill produce? ")))), "items p/sec."
-        elif asked == 5: result = "The drill will produce", str(MPDBoost(float(input("How much does the drill produce? ")))), "items p/sec."
+        elif asked == 1: result = "You can run " + str(pyraFacToBlastFac(int(input("How many pyratite factories? ")))) + " blast factories."
+        elif asked == 2: result = "You can run " + str(blastFacToImpact(int(input("How many blast factories? ")))) + " reactors."
+        elif asked == 3: result = "You can run " + str(blastFacToImpact(pyraFacToBlastFac(int(input("How many pyratite factories? "))))) + " reactors."   
+        elif asked == 4: result = "The drill will produce " + str(goliathBoost(float(input("How much does the drill produce? ")))) + " items p/sec."
+        elif asked == 5: result = "The drill will produce " + str(MPDBoost(float(input("How much does the drill produce? ")))) + " items p/sec."
         else: 
             print("Please select an option. Input 0 to go back.")
             askExogenesis()
